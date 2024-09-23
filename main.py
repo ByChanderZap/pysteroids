b7f404507df2dc94b77f8c880dd9c1c2a84399ce
alexander herrera <chander.zap@gmail.com>
2024-09-23 12:39:16 -0600

    [add] - finished base project and added extra speed

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import pygame
-from player import Player
+import sys
 
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  running = True
   clock = pygame.time.Clock()
-  
-  dt = 0
+
   updatable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
+  shots = pygame.sprite.Group()
+
+  Asteroid.containers = (asteroids, updatable, drawable)
+  Shot.containers = (shots, updatable, drawable)
+  AsteroidField.containers = updatable
+  asteroid_field = AsteroidField()
 
   Player.containers = (updatable, drawable)
 
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+  dt = 0
 
   while True:
     for event in pygame.event.get():
@@ -24,6 +36,15 @@ def main():
 
     for obj in updatable:
       obj.update(dt)
+
+    for asteroid in asteroids:
+      if asteroid.collides_with(player):
+        print("Game over!")
+        sys.exit()
+      for shot in shots:
+        if asteroid.collides_with(shot):
+          asteroid.split()
+          shot.kill()
 
     screen.fill("black")
 
@@ -35,6 +56,6 @@ def main():
     # limit the framerate to 60 FPS
     dt = clock.tick(60) / 1000
 
-if __name__ == "__main__":
-  main()
 
+if __name__ == "__main__":
+    main()
