fcd02fbc51f8bb30b44074fe2bb4b152c6cb0c49
alexander herrera <chander.zap@gmail.com>
2024-09-22 18:59:53 -0600

    [add] use of groups and added movement to player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 
 from constants import *
 
@@ -6,20 +7,33 @@ def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   running = True
+  clock = pygame.time.Clock()
+  
+  dt = 0
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
 
-  while running:
+  Player.containers = (updatable, drawable)
+
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+  while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
 
-    screen.fill('black')
+    for obj in updatable:
+      obj.update(dt)
+
+    screen.fill("black")
+
+    for obj in drawable:
+      obj.draw(screen)
+
     pygame.display.flip()
-  
 
-  print('Starting asteroids!')
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
-
+    # limit the framerate to 60 FPS
+    dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
   main()
